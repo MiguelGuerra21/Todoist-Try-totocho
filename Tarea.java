934f5189d1d9d58fd46318517f1239ea1d274a4d
Miguel Guerra <migueclase2@gmail.com>
2016-12-15 10:43:20 +0100

    descripcion de la tarea al crearla

---
+++ Tarea.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Tarea here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tarea
+{
+    // instance variables - replace the example below with your own
+    private String descripcion;
+    
+
+    /**
+     * Constructor for objects of class Tarea
+     */
+    public Tarea(String descripcion)
+    {
+        this.descripcion = descripcion;
+    }
+    
+}
